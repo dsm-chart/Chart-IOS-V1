a31a3a98f8367780ee91023d02211c7df139903e
김대희 <0824dh@naver.com>
2022-04-18 17:02:49 +0900

    🌱 :: 아, 점, 저 View UI

--- Chart-IOS/Sources/VC/SchoolMealVC.swift
+++ Chart-IOS/Sources/VC/SchoolMealVC.swift
@@ -5,8 +5,48 @@
 //  Created by 김대희 on 2022/04/17.
 //
 
-import Foundation
+import UIKit
 
 class SchoolMealVC: BaseViewController {
+    
+    private let breakfastView = UIView()
+    private let lunchView = UIView()
+    private let dinnerView = UIView()
+    
+    
+    func makeMealView() {
+        [breakfastView, lunchView, dinnerView].forEach {
+            $0.backgroundColor = Asset.viewColor.color
+            $0.layer.cornerRadius = 20
+        }
+    }
+    
+    func addSubView() {
+        [breakfastView, lunchView, dinnerView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func configureUI() {
+        makeMealView()
+        addSubView()
+    }
+    
+    override func setupConstraints() {
+        [breakfastView, lunchView, dinnerView].forEach { view in
+            view.snp.makeConstraints {
+                $0.height.equalTo(140)
+                $0.left.right.equalTo(self.view).inset(35)
+            }
+        }
+        lunchView.snp.makeConstraints { $0.centerY.centerX.equalTo(view) }
+        breakfastView.snp.makeConstraints {
+            $0.bottom.equalTo(self.lunchView.snp.top).offset(-25)
+        }
+        dinnerView.snp.makeConstraints {
+            $0.top.equalTo(self.lunchView.snp.bottom).offset(25)
+        }
+        
+    }
     
 }
