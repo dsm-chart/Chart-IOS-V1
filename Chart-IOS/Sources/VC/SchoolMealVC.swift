0e492fe38897e78e47c256fa448ff9a5ca5c346a
김대희 <0824dh@naver.com>
2022-04-18 19:04:35 +0900

    🌧 :: MealNameLabel UI 추가

--- Chart-IOS/Sources/VC/SchoolMealVC.swift
+++ Chart-IOS/Sources/VC/SchoolMealVC.swift
@@ -13,17 +13,36 @@ class SchoolMealVC: BaseViewController {
     private let lunchView = UIView()
     private let dinnerView = UIView()
     
+    private let breakfastNameLabel = UILabel()
+    private let lunchNameLabel = UILabel()
+    private let dinnerNameLabel = UILabel()
+
     
     func makeMealView() {
         [breakfastView, lunchView, dinnerView].forEach {
             $0.backgroundColor = Asset.viewColor.color
             $0.layer.cornerRadius = 20
         }
+        
+        [breakfastNameLabel, lunchNameLabel, dinnerNameLabel].forEach {
+            $0.textColor = Asset.mainColor.color
+            $0.font = .roundedFont(ofSize: 15, weight: .semibold)
+        }
+
     }
     
     func addSubView() {
-        [breakfastView, lunchView, dinnerView].forEach {
-            view.addSubview($0)
+        
+        let mealName =  ["아침", "점심", "저녁"]
+        
+        let mealViewArray = [breakfastView, lunchView, dinnerView]
+        let mealNameLabelArray = [breakfastNameLabel, lunchNameLabel, dinnerNameLabel]
+        
+        for count in 0..<3 {
+            mealNameLabelArray[count].text = mealName[count]
+            view.addSubview(mealViewArray[count])
+            mealViewArray[count].addSubview(mealNameLabelArray[count])
+            
         }
     }
     
@@ -33,6 +52,7 @@ class SchoolMealVC: BaseViewController {
     }
     
     override func setupConstraints() {
+        
         [breakfastView, lunchView, dinnerView].forEach { view in
             view.snp.makeConstraints {
                 $0.height.equalTo(140)
@@ -45,6 +65,13 @@ class SchoolMealVC: BaseViewController {
         }
         dinnerView.snp.makeConstraints {
             $0.top.equalTo(self.lunchView.snp.bottom).offset(25)
+        }
+        
+        [breakfastNameLabel, lunchNameLabel, dinnerNameLabel].forEach { label in
+            label.snp.makeConstraints {
+                $0.top.left.equalTo(15)
+                $0.height.equalTo(20)
+            }
         }
         
     }
