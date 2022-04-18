45bfe71fe0dc7d53681c22a30aa5e222d3617e32
김대희 <0824dh@naver.com>
2022-04-18 17:22:03 +0900

    🌱 :: Font.swift

---
+++ Chart-IOS/Resources/Extension/Font.swift
@@ -0,0 +1,28 @@
+//
+//  Font.swift
+//  Chart-IOS
+//
+//  Created by 김대희 on 2022/04/18.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    static func roundedFont(ofSize fontSize: CGFloat, weight: UIFont.Weight) -> UIFont {
+        let systemFont = UIFont.systemFont(ofSize: fontSize, weight: weight)
+        let font: UIFont
+        
+        if #available(iOS 13.0, *) {
+            if let descriptor = systemFont.fontDescriptor.withDesign(.rounded) {
+                font = UIFont(descriptor: descriptor, size: fontSize)
+            } else {
+                font = systemFont
+            }
+        } else {
+            font = systemFont
+        }
+        return font
+    }
+    
+}
