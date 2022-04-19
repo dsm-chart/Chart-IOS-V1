32224d78b976fe368da7c291600fab9436ecad3d
김대희 <0824dh@naver.com>
2022-04-19 23:09:25 +0900

    🌧 :: TextField, Label 등 선언

--- Chart-IOS/Sources/VC/Login/SignInVC.swift
+++ Chart-IOS/Sources/VC/Login/SignInVC.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+class SignInVC: BaseViewController {
+    
+    private let signInNameLabbel = UILabel()
+    private let signInDoneButton = UIButton()
+    
+    private let schoolNameTextField = UITextField()
+    private let schoolGradeTextField = UITextField()
+    private let schoolClassNumberTextField = UITextField()
+    
+    private let textFieldBackView1 = UIView()
+    private let textFieldBackView2 = UIView()
+    private let textFieldBackView3 = UIView()
+    
+    func addSubView() {
+        
+    }
+        
+    override func configureUI() {
+        signInDoneButton.makeMyDesign(color: Asset.mainColor.color, title: "완료", titleColor: .white)
+        addSubView()
+    }
+    
+}
