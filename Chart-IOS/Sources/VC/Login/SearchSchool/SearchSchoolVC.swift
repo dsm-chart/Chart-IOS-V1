d99019791d512dfd1c1ad213ff8c241131f543ac
김대희 <0824dh@naver.com>
2022-04-20 22:10:10 +0900

    🌧 :: SearchView TextField UI

--- Chart-IOS/Sources/VC/Login/SearchSchool/SearchSchoolVC.swift
+++ Chart-IOS/Sources/VC/Login/SearchSchool/SearchSchoolVC.swift
@@ -26,6 +26,10 @@ class SearchSchoolVC: BaseViewController {
         $0.borderStyle = .none
         $0.font = .roundedFont(ofSize: 18, weight: .medium)
         $0.tintColor = Asset.mainColor.color
+        $0.attributedPlaceholder = NSAttributedString(string: "학교를 검색하세요", attributes: [
+            NSAttributedString.Key.foregroundColor: UIColor.secondaryLabel,
+            NSAttributedString.Key.font : UIFont.roundedFont(ofSize: 18, weight: .medium)
+        ])
     }
     
     let tableView = UITableView().then {
@@ -40,9 +44,22 @@ class SearchSchoolVC: BaseViewController {
         [textFieldBackShadowView, textFieldBackView, tableView].forEach {
             view.addSubview($0)
         }
-
-        textFieldBackShadowView.layer.addSublayer(gradintLayer)
+        
+        textFieldBackView.addSubview(schoolSearchTextField)
+//        textFieldBackShadowView.layer.addSublayer(gradintLayer)
+    }
     
+    override func setupConstraints() {
+        textFieldBackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(25)
+            $0.height.equalTo(50)
+            $0.leading.trailing.equalTo(view).inset(25)
+        }
+        schoolSearchTextField.snp.makeConstraints {
+            $0.leading.trailing.equalTo(textFieldBackView).inset(10)
+            $0.top.bottom.equalTo(textFieldBackView).inset(15)
+        }
+        
     }
 
 }
