e5b8977457275ec5deaab147c5f592cc54cd3f9d
Daehee Kim <0824dh@naver.com>
2022-04-28 11:43:41 +0900

    🌧 :: DoneTextLabel

--- Chart-IOS/Sources/VC/Login/SiginInDoneVC.swift
+++ Chart-IOS/Sources/VC/Login/SiginInDoneVC.swift
@@ -22,6 +22,13 @@ class SiginInDoneVC: BaseViewController {
         $0.animationDuration = 1
     }
     
+    private let doneTextLabel = UILabel().then {
+        $0.text = "회원가입이 성공적으로 완료되었어요"
+        $0.font = .roundedFont(ofSize: 20, weight: .semibold)
+        $0.textAlignment = .center
+        $0.textColor = .clear
+    }
+    
     private let maskView = UIView()
     private let goHomeButton = UIButton()
     
@@ -29,7 +36,7 @@ class SiginInDoneVC: BaseViewController {
         
         maskView.backgroundColor = .clear
         goHomeButton.makeMyDesign(color: Asset.mainColor.color, title: "첫 화면으로 돌아가기", titleColor: .white)
-        [doneBox, goHomeButton, maskView].forEach {
+        [doneBox, goHomeButton, maskView, doneTextLabel].forEach {
             view.addSubview($0)
         }
         
@@ -41,7 +48,7 @@ class SiginInDoneVC: BaseViewController {
             $0.width.height.equalTo(200)
             $0.centerX.equalTo(view)
             $0.centerY.equalTo(view).offset(-130)
-
+            
         }
         doneBox.snp.makeConstraints {
             $0.width.height.equalTo(150)
@@ -55,11 +62,26 @@ class SiginInDoneVC: BaseViewController {
             $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-5)
         }
         
+        doneTextLabel.snp.makeConstraints {
+            $0.top.equalTo(doneBox.snp.bottom).offset(30)
+            $0.centerX.equalTo(view)
+            $0.height.equalTo(40)
+        }
+        
     }
     
     override func setLayout() {
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             self.doneBox.setOn(true, animated: true)
+            
+            UIView.transition(with: self.doneTextLabel,
+                              duration: 0.5,
+                              options: .transitionCrossDissolve,
+                              animations: { [weak self] in
+                self?.doneTextLabel.textColor = Asset.labelColor.color
+            }, completion: nil)
+
         }
     }
     
