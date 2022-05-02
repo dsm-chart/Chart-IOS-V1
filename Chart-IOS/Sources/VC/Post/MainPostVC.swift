e1ac7076ff72b6fbb81878c29dae7b83ff9195bd
Daehee Kim <0824dh@naver.com>
2022-05-02 15:04:52 +0900

    🌱 :: MDCFloatingButton 추가

--- Chart-IOS/Sources/VC/Post/MainPostVC.swift
+++ Chart-IOS/Sources/VC/Post/MainPostVC.swift
@@ -5,10 +5,28 @@
 //  Created by 김대희 on 2022/04/17.
 //
 
-import Foundation
+import UIKit
 import MaterialComponents.MaterialButtons
 import MaterialComponents.MaterialButtons_Theming
 
 class MainPostVC: BaseViewController {
+
+    private let createPostFloatingButton = MDCFloatingButton(shape: .default).then {
+        $0.setBackgroundColor(Asset.mainColor.color)
+        $0.setImage(UIImage(systemName: "square.and.pencil"), for: .normal)
+        $0.setImageTintColor(.white, for: .normal)
+    }
+
+    override func configureUI() {
+        [createPostFloatingButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    override func setupConstraints() {
+        createPostFloatingButton.snp.makeConstraints {
+            $0.trailing.bottom.equalTo(view.safeAreaLayoutGuide).offset(-25)
+        }
+    }
     
 }
