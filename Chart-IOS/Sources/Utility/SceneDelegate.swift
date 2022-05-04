c0328df1b60770d1e7e6e00c5ddfba91baef5655
Daehee Kim <0824dh@naver.com>
2022-05-04 23:36:11 +0900

    🌧 ::  BaseNavigationController 적용

--- Chart-IOS/Sources/Utility/SceneDelegate.swift
+++ Chart-IOS/Sources/Utility/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene) // SceneDelegate의 프로퍼티에 설정해줌
-        let mainViewController = UINavigationController(rootViewController: LoginVC()) // First ViewController
+        let mainViewController = BaseNavigationController(rootViewController: LoginVC()) // First ViewController
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
         guard let _ = (scene as? UIWindowScene) else { return }
