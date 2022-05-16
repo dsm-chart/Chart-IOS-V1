dbf50a50f5ad7682fd945598d6538419dfd53cd9
Daehee Kim <0824dh@naver.com>
2022-05-16 15:38:34 +0900

    🌱 :: titleLabel, titleTextField, contentTextView 생성

--- Chart-IOS/Sources/VC/Post/AddPostVC.swift
+++ Chart-IOS/Sources/VC/Post/AddPostVC.swift
@@ -7,6 +7,35 @@
 
 import UIKit
 
-class AddPostVC; BaseViewController {
+class AddPostVC: BaseViewController {
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = Asset.mainColor.color
+        $0.text = "제목 :"
+    }
+
+    fileprivate let titleTextField = UITextField().then {
+        $0.borderStyle = .none
+        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.tintColor = Asset.mainColor.color
+        $0.attributedPlaceholder = NSAttributedString(string: "제목을 입력하세요", attributes:[
+            NSAttributedString.Key.foregroundColor: UIColor.separator,
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 20, weight: .semibold)
+        ])
+    }
+    
+    let textViewPlaceHolder = "여기에 본문을 입력해 주세요"
+    
+    fileprivate lazy var contentTextView = UITextView().then {
+        $0.tintColor = Asset.mainColor.color
+        $0.font = .roundedFont(ofSize: 18, weight: .regular)
+        $0.text = textViewPlaceHolder
+        $0.textColor = .separator
+    }
+    
+    override func configureUI() {
+        contentTextView.delegate = self
+    }
     
 }
