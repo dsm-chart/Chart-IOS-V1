2db5a5fbb589849d79d4be40524cb862d44f13e5
Daehee Kim <0824dh@naver.com>
2022-05-16 21:57:09 +0900

    🌱 :: titleLabel layout

--- Chart-IOS/Sources/VC/Post/AddPostVC.swift
+++ Chart-IOS/Sources/VC/Post/AddPostVC.swift
@@ -12,7 +12,7 @@ class AddPostVC: BaseViewController {
     private let titleLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 20, weight: .semibold)
         $0.textColor = Asset.mainColor.color
-        $0.text = "제목 :"
+        $0.text = "제목 : "
     }
 
     private let titleTextField = UITextField().then {
@@ -36,11 +36,31 @@ class AddPostVC: BaseViewController {
     
     override func configureUI() {
         contentTextView.delegate = self
-        self.navigationItem.title = "새 글 작성하기"
+        navigationItem.title = "새 글 작성하기"
         [titleLabel, titleTextField, contentTextView].forEach {
             view.addSubview($0)
         }
 
+    }
+
+    override func setupConstraints () {
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.equalTo(view.safeAreaLayoutGuide).offset(15)
+            $0.height.equalTo(35)
+            $0.width.equalTo(45)
+        }
+        titleTextField.snp.makeConstraints {
+            $0.leading.equalTo(titleLabel.snp.trailing).offset(15)
+            $0.height.equalTo(35)
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(15)
+            $0.trailing.equalTo(-15)
+        }
+
+        contentTextView.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view).inset(15)
+            $0.top.equalTo(titleTextField.snp.bottom).offset(15)
+            $0.bottom.equalToSuperview()
+        }
     }
 }
 
