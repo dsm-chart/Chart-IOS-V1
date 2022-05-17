812912a06cfdaa06fe841e3a302ac425779d0c9d
Daehee Kim <0824dh@naver.com>
2022-05-17 12:59:09 +0900

    🌱 :: tabBar.isHidden

--- Chart-IOS/Sources/VC/Post/AddPostVC.swift
+++ Chart-IOS/Sources/VC/Post/AddPostVC.swift
@@ -8,65 +8,70 @@
 import UIKit
 
 class AddPostVC: BaseViewController {
-    
-    private let titleLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+
+    private let titleLabel = UILabel ().then {
+        $0.font = .systemFont (ofSize: 20, weight: .semibold)
         $0.textColor = Asset.mainColor.color
         $0.text = "제목 : "
     }
 
-    private let titleTextField = UITextField().then {
+    private let titleTextField = UITextField ().then {
         $0.borderStyle = .none
-        $0.font = .systemFont(ofSize: 20, weight: .semibold)
+        $0.font = .systemFont (ofSize: 20, weight: .semibold)
         $0.tintColor = Asset.mainColor.color
     }
-    
+
     fileprivate let textViewPlaceHolder = "여기에 본문을 입력해 주세요"
-    
-    private lazy var contentTextView = UITextView().then {
+
+    private lazy var contentTextView = UITextView ().then {
         $0.tintColor = Asset.mainColor.color
-        $0.font = .roundedFont(ofSize: 18, weight: .regular)
+        $0.font = .roundedFont (ofSize: 18, weight: .regular)
         $0.text = textViewPlaceHolder
         $0.textColor = .separator
         $0.backgroundColor = .clear
     }
-    
-    override func configureUI() {
+
+    override func setLayout () {
+        self.tabBarController?.tabBar.isHidden = true
+    }
+
+    override func configureUI () {
         contentTextView.delegate = self
         navigationItem.title = "새 글 작성하기"
         [titleLabel, titleTextField, contentTextView].forEach {
-            view.addSubview($0)
+            view.addSubview ($0)
         }
     }
 
     override func setupConstraints () {
         titleLabel.snp.makeConstraints {
-            $0.top.leading.equalTo(view.safeAreaLayoutGuide).offset(15)
-            $0.height.equalTo(35)
-            $0.width.equalTo(45)
+            $0.top.leading.equalTo (view.safeAreaLayoutGuide).offset (15)
+            $0.height.equalTo (35)
+            $0.width.equalTo (45)
         }
         titleTextField.snp.makeConstraints {
-            $0.leading.equalTo(titleLabel.snp.trailing).offset(15)
-            $0.height.equalTo(35)
-            $0.top.equalTo(view.safeAreaLayoutGuide).offset(15)
-            $0.trailing.equalTo(-15)
+            $0.leading.equalTo (titleLabel.snp.trailing).offset (15)
+            $0.height.equalTo (35)
+            $0.top.equalTo (view.safeAreaLayoutGuide).offset (15)
+            $0.trailing.equalTo (-15)
         }
         contentTextView.snp.makeConstraints {
-            $0.leading.trailing.equalTo(view).inset(15)
-            $0.top.equalTo(titleTextField.snp.bottom).offset(15)
-            $0.bottom.equalToSuperview()
+            $0.leading.trailing.equalTo (view).inset (15)
+            $0.top.equalTo (titleTextField.snp.bottom).offset (15)
+            $0.bottom.equalToSuperview ()
         }
     }
 }
 
 extension AddPostVC: UITextViewDelegate {
-    func textViewDidBeginEditing(_ textView: UITextView) {
+    func textViewDidBeginEditing (_ textView: UITextView) {
         if textView.text == textViewPlaceHolder {
             textView.text = nil
             textView.textColor = Asset.labelColor.color
         }
     }
-    func textViewDidEndEditing(_ textView: UITextView) {
+
+    func textViewDidEndEditing (_ textView: UITextView) {
         if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             textView.text = textViewPlaceHolder
             textView.textColor = .separator
