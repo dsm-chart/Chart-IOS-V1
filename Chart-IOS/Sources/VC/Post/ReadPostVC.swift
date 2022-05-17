a18adb42ceaefedf74bdb2124516628f39dcd163
Daehee Kim <0824dh@naver.com>
2022-05-17 23:25:54 +0900

    🌱 :: goCommentButton

--- Chart-IOS/Sources/VC/Post/ReadPostVC.swift
+++ Chart-IOS/Sources/VC/Post/ReadPostVC.swift
@@ -30,14 +30,20 @@ class ReadPostVC: BaseViewController {
         tabBarController?.tabBar.isHidden = true
     }
 
+    private let goCommentButton = UIButton()
+
     override func configureUI() {
+
+        // Dummy Data
         navigationItem.title = "OOO님의 개시물"
         titleTextLabel.text = "제목이 들어가요."
         dateLabel.text = "0000-00-00 시간에 OOO님이 작성했어요."
         contentTextView.text = "여기에는 본문이 들어가고 곧 삭제될 더미데이터 에여 ㅎㅎ"
-        [titleTextLabel, dateLabel, contentTextView].forEach {
+
+        [titleTextLabel, dateLabel, contentTextView, goCommentButton].forEach {
             view.addSubview($0)
         }
+        goCommentButton.makeMyDesign(color: Asset.mainColor.color, title: "Comment 보기", titleColor: .white)
     }
 
     override func setupConstraints() {
@@ -55,6 +61,11 @@ class ReadPostVC: BaseViewController {
             $0.leading.trailing.equalTo(view).inset(15)
             $0.top.equalTo(dateLabel.snp.bottom).offset(15)
             $0.bottom.equalToSuperview()
+        }
+        goCommentButton.snp.makeConstraints {
+            $0.leading.trailing.equalTo(view).inset(15)
+            $0.height.equalTo(50)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-5)
         }
     }
 
