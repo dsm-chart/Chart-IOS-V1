9e8affc555953e2d2da8fa3f4e1ed37d46381383
Daehee Kim <0824dh@naver.com>
2022-05-19 11:54:24 +0900

    🌱 :: periodNumberStackView, dayStringStackView 레이아웃, 값 넣어주기

--- Chart-IOS/Sources/VC/TimeTable/TimeTableVC.swift
+++ Chart-IOS/Sources/VC/TimeTable/TimeTableVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class TimeTableVC: BaseViewController {
-    
+
     private let semesterBackView = UILabel().then { $0.backgroundColor = .clear }
 
     private let backLine1 = UIView()
@@ -19,27 +19,28 @@ class TimeTableVC: BaseViewController {
         $0.font = .roundedFont(ofSize: 15, weight: .semibold)
         $0.textAlignment = .center
     }
-    
+
     private let classNameLabel = UILabel().then {
         $0.textColor = Asset.labelColor.color
         $0.font = .roundedFont(ofSize: 20, weight: .semibold)
         $0.textAlignment = .center
     }
-    
+
     private let schoolTimeTableView = UIView().then {
         $0.backgroundColor = Asset.viewColor.color
         $0.layer.cornerRadius = 20
     }
 
     private let periodNumberStackView = UIStackView().then {
+        $0.distribution = .fillEqually
         $0.axis = .vertical
     }
 
     private let dayStringStackView = UIStackView().then {
+        $0.distribution = .fillEqually
         $0.axis = .horizontal
     }
 
-    
     override func configureUI() {
 
         [backLine1, backLine2].forEach { $0.backgroundColor = Asset.mainColor.color }
@@ -54,33 +55,59 @@ class TimeTableVC: BaseViewController {
         [backLine1, backLine2, periodNumberStackView, dayStringStackView].forEach {
             schoolTimeTableView.addSubview($0)
         }
-        
+
         /// dummy Data
         semesterLabel.text = "0000년 X학기"
         classNameLabel.text = "0-0 시간표"
-        
+        makeStackView()
     }
-    
+
+
+    private func makeStackView() {
+
+        let poeriodArray = ["월", "화", "수", "목", "금"]
+
+        for number in 0..<5 {
+            let periodLabel = UILabel().then {
+                $0.textAlignment = .center
+                $0.font = .systemFont(ofSize: 18, weight: .bold)
+                $0.textColor = .secondaryLabel
+                $0.text = "\(poeriodArray[number])"
+            }
+            dayStringStackView.addArrangedSubview(periodLabel)
+        }
+
+        for number in 1..<8 {
+            let dayLabel = UILabel().then {
+                $0.textAlignment = .center
+                $0.font = .systemFont(ofSize: 18, weight: .bold)
+                $0.textColor = .secondaryLabel
+                $0.text = "\(number)"
+            }
+            periodNumberStackView.addArrangedSubview(dayLabel)
+        }
+    }
+
     override func setupConstraints() {
-        
+
         semesterBackView.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview()
             $0.bottom.equalTo(schoolTimeTableView.snp.top)
         }
-        
+
         semesterLabel.snp.makeConstraints {
             $0.height.equalTo(35)
             $0.centerX.equalTo(view)
             $0.bottom.equalTo(classNameLabel.snp.top)
         }
-        
+
         classNameLabel.snp.makeConstraints {
             $0.height.equalTo(35)
             $0.centerX.equalTo(view)
             $0.centerY.equalTo(semesterBackView).offset(20)
         }
-        
+
         schoolTimeTableView.snp.makeConstraints {
             $0.height.equalTo(469)
             $0.left.right.equalTo(self.view).inset(35)
@@ -92,7 +119,6 @@ class TimeTableVC: BaseViewController {
             $0.top.bottom.equalTo(schoolTimeTableView).inset(20)
             $0.width.equalTo(1)
         }
-
         backLine2.snp.makeConstraints {
             $0.left.right.equalTo(schoolTimeTableView).inset(20)
             $0.top.equalTo(55)
@@ -100,16 +126,19 @@ class TimeTableVC: BaseViewController {
         }
 
         periodNumberStackView.snp.makeConstraints {
-            $0.left.equalTo(50)
-            $0.right.equalTo(-20)
-            $0.height.equalTo(35)
-            $0.bottom.equalTo(backLine2)
-        }
-        dayStringStackView.snp.makeConstraints {
             $0.right.equalTo(backLine1)
             $0.top.equalTo(backLine2)
             $0.bottom.equalTo(-20)
             $0.width.equalTo(30)
         }
+        dayStringStackView.snp.makeConstraints {
+            $0.left.equalTo(50)
+            $0.right.equalTo(-20)
+            $0.height.equalTo(35)
+            $0.bottom.equalTo(backLine2)
+        }
+
     }
 }
+
+
