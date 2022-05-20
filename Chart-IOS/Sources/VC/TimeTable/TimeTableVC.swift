8a1386c6e630220008f824b7bd12edbdf2c25157
Daehee Kim <0824dh@naver.com>
2022-05-20 15:28:47 +0900

    🌱 :: MakeStackView

--- Chart-IOS/Sources/VC/TimeTable/TimeTableVC.swift
+++ Chart-IOS/Sources/VC/TimeTable/TimeTableVC.swift
@@ -66,9 +66,64 @@ class TimeTableVC: BaseViewController {
         /// dummy Data
         semesterLabel.text = "0000년 X학기"
         classNameLabel.text = "0-0 시간표"
-        
+        makeStackView()
     }
-    
+
+
+    private func makeStackView() {
+
+        let periodArray = ["월", "화", "수", "목", "금"]
+
+        for number in 0..<5 {
+            let periodLabel = UILabel().then {
+                $0.textAlignment = .center
+                $0.font = .systemFont(ofSize: 18, weight: .bold)
+                $0.textColor = .separator
+                $0.text = "\(periodArray[number])"
+            }
+            dayStringStackView.addArrangedSubview(periodLabel)
+        }
+
+        for number in 1..<8 {
+            let dayLabel = UILabel().then {
+                $0.textAlignment = .center
+                $0.font = .systemFont(ofSize: 18, weight: .bold)
+                $0.textColor = .separator
+                $0.text = "\(number)"
+            }
+            periodNumberStackView.addArrangedSubview(dayLabel)
+        }
+
+        // Dummy Data
+        let mondayArr = ["음악", "사회", "대베", "성직", "문학", "자바", "자바"]
+        let tuesdayArr = ["음악", "문학", "자바", "자바", "운체", "사회", "대베"]
+        let wednesdayArr = ["자바", "자바", "데베", "제베", "운체", "자바", "대희"]
+        let thursdayArr = ["음악", "문학", "자바", "자바", "운체", "사회", ""]
+        let fridayArr = ["자바", "자바", "데베", "제베", "운체", "", "", ""]
+
+        let weekStackViewArray = [mondayStackView, tuesdayStackView, wednesdayStackView, thursdayStackView, fridayStackView]
+        let weekValueArray = [mondayArr, tuesdayArr, wednesdayArr, thursdayArr, fridayArr]
+
+        for weekNumber in 0..<5 {
+            for dayNumber in 0..<7 {
+                let weekArr = weekValueArray[weekNumber]
+                let label = UILabel().then {
+                    $0.textColor = Asset.labelColor.color
+                    $0.text = "\(weekArr[dayNumber])"
+                    $0.textAlignment = .center
+                }
+                weekStackViewArray[weekNumber].addArrangedSubview(label)
+            }
+        }
+
+        weekStackViewArray.forEach {
+            $0.distribution = .fillEqually
+            $0.axis = .vertical
+            timeTableStackView.addArrangedSubview($0)
+        }
+
+    }
+
     override func setupConstraints() {
         
         semesterBackView.snp.makeConstraints {
