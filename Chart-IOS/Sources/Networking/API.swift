443f8d0bf5c8d43acd5028b9cb269fbdb50b036a
Daehee Kim <0824dh@naver.com>
2022-05-24 23:31:35 +0900

    🌧 :: api enum case 작성

--- Chart-IOS/Sources/Networking/API.swift
+++ Chart-IOS/Sources/Networking/API.swift
@@ -6,7 +6,35 @@
 //
 
 import Foundation
+import Moya
 
 enum API {
-    
+
+    // question-controller
+
+    case postQuestion(_ title: String, _ content: String)
+    case getQuestion(_ idx: Int, _ size: Int)
+
+    // question-controller
+
+    case postComment(_ content: String, _ targetId: String)
+    case deleteComment(_ commentId: String)
+    case patchComment(_ content: String, _ targetId: String)
+
+    // account-controller
+
+    case signUp
+    case reissue
+    case login(_ githubCode: String)
+    case myAuth
+
+    // timetable-controller
+
+    case getTimeTable(_ start: Int, _ end: Int)
+    case todayTimeTable
+
+    // cafeteria-controller
+
+    case getMeal
+
 }
