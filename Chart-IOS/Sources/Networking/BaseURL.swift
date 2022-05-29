dccad0566182035a0efae223fab1d1184c0ee9cd
Daehee Kim <0824dh@naver.com>
2022-05-29 21:08:14 +0900

    🌧 :: github Base URL&Key

--- Chart-IOS/Sources/Networking/BaseURL.swift
+++ Chart-IOS/Sources/Networking/BaseURL.swift
@@ -11,4 +11,7 @@ struct Base {
     static let baseURL = "http://13.125.236.178:8080"
     static let neisURL = "https://open.neis.go.kr"
     static let neisApiKey = "f28891c644a84ae5a968e48ea8e2f9d6"
+    static let githubURL = "https://github.com"
+    static let githubScope = "repo,user"
+    static let githubClientId = "89bb7ffb7b5121039716"
 }
