aabbb7d915b7616cc8b631524beb4a4e570f7f66
Daehee Kim <0824dh@naver.com>
2022-06-02 11:39:21 +0900

    🌧 :: SignInReactor Action, Mutatiom, State

--- Chart-IOS/Sources/Scene/Login/Reactor/SignInReactor.swift
+++ Chart-IOS/Sources/Scene/Login/Reactor/SignInReactor.swift
@@ -12,4 +12,25 @@ import RxCocoa
 
 final class SignInReactor: Reactor {
     
+    private let disposeBag: DisposeBag = .init()
+    
+    enum Action {
+        case updateSchoolClass(Int)
+        case updateSchoolNumber(Int)
+        case searchSchoolDidTap(String)
+        case signUpButtonDidTap
+    }
+    
+    enum Mutation {
+        case setSchoolClass(Int)
+        case setSchoolNumber(Int)
+        case getschoolList([SearchRow])
+    }
+    
+    struct State  {
+        var schoolClass: Int
+        var scholNumber: Int
+        var schoolCode: String
+        var areaCode: String
+    }
 }
