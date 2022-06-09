ad5aeec19ac7dcec7151865b1a427a69a259a158
Daehee Kim <0824dh@naver.com>
2022-06-09 23:54:19 +0900

    🌧 :: Login 유효성 검사 로직 수정

--- Chart-IOS/Sources/Utility/SceneDelegate.swift
+++ Chart-IOS/Sources/Utility/SceneDelegate.swift
@@ -11,26 +11,28 @@ import SPAlert
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
+    let disposeBag = DisposeBag()
+    
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-        if KeyChain.read(key: Token.refreshToken) == nil {
-            goLogin()
-        } else {
-            goMainHome()
-        }
+        if KeyChain.read(key: Token.refreshToken) != nil {
+            LoginManager.shared.postReissue().bind { bool in
+                if bool == true { self.goMainHome() }
+            }.disposed(by: disposeBag)
+        } else { goLogin() }
         
         window?.makeKeyAndVisible()
         
-        #if targetEnvironment(macCatalyst)
+#if targetEnvironment(macCatalyst)
         if let titlebar = windowScene.titlebar {
             titlebar.titleVisibility = .hidden
             titlebar.toolbar = nil
         }
-        #endif
+#endif
         
     }
     
@@ -52,7 +54,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
         
-        let disposeBag = DisposeBag()
         if let url = URLContexts.first?.url {
             print("URLContexts: \(url)")
             if url.absoluteString.starts(with: "githubprviewer://") {
@@ -63,16 +64,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                         if bool == true {
                             LoginManager.shared.checkGithubUser().bind { string in
                                 if string == "true" {
+                                    print(string)
                                     LoginManager.shared.login().bind { bool in
+                                        print(bool)
                                         if bool == true { self.presentAlert("로그인 성공!", .done) }
                                         if bool == false { self.presentAlert("로그인 실패!", .error) }
-                                    }.disposed(by: disposeBag)
+                                    }.disposed(by: self.disposeBag)
                                 } else if string == "false" {
                                     let mainViewController = BaseNavigationController(rootViewController: LoginVC())
                                     mainViewController.pushViewController(SignInVC(), animated: true)
                                     self.window?.rootViewController = mainViewController
                                 } else { self.presentAlert("알 수 없는 오류 발생!", .error) }
-                            }.disposed(by: disposeBag)
+                            }.disposed(by: self.disposeBag)
                         }
                     }.disposed(by: disposeBag)
                 }
