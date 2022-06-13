b7524cd94b98370fc57f53d6d5e6036a89eab5bd
Daehee Kim <0824dh@naver.com>
2022-06-13 17:17:00 +0900

    🌧 :: MealReactor 나머지

--- Chart-IOS/Sources/Scene/SchoolMeal/SchoolMealReactor.swift
+++ Chart-IOS/Sources/Scene/SchoolMeal/SchoolMealReactor.swift
@@ -15,16 +15,58 @@ class SchoolMealReactor: Reactor {
     
     let initialState = State(schoolList: .init(breakfast: [], lunch: [], dinner: []))
     private let disposeBag: DisposeBag = .init()
-
+    
     enum Action {
         case viewDidLoad
     }
     enum Mutation {
         case getSchoolMeal(Meal)
     }
-    
     struct State {
         var schoolList: Meal
     }
 }
 
+// MARK: - Mutate
+extension SchoolMealReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidLoad:
+            let  getSchoolMeal = getMeal().map { meal -> Mutation in
+                return .getSchoolMeal(meal)
+            }
+            return getSchoolMeal
+        }
+    }
+}
+
+// MARK: - Reduce
+extension SchoolMealReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .getSchoolMeal(meal):
+            newState.schoolList = meal
+        }
+        return newState
+    }
+}
+
+// MARK: - Function
+
+extension SchoolMealReactor {
+    func getMeal() -> PublishRelay<Meal> {
+        let meal = PublishRelay<Meal>()
+        API.getMeal.request().subscribe { event in
+            switch event {
+            case .success(let response):
+                if let data = try? JSONDecoder().decode(Meal.self, from: response.data) {
+                    meal.accept(data)
+                }
+            case .failure(_):
+                SPIndicator.present(title: "Error!!", haptic: .error)
+            }
+        }.disposed(by: disposeBag)
+        return meal
+    }
+}
