21fad6547b62cfb5d9a275718fd7d806f7fb8434
Daehee Kim <0824dh@naver.com>
2022-06-16 10:03:33 +0900

    🌧 :: MainPostVC bind

--- Chart-IOS/Sources/Scene/Post/UI/MainPostVC.swift
+++ Chart-IOS/Sources/Scene/Post/UI/MainPostVC.swift
@@ -7,11 +7,18 @@
 
 import UIKit
 import MaterialComponents.MaterialButtons
+import ReactorKit
+
+class MainPostVC: BaseViewController, View {
+    
+    let reactor = MainPostReactor()
+    
     let dateFormatter = DateFormatter().then {
         $0.dateFormat = "YYYY년 MM월 dd일"
     }
 
-class MainPostVC: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+    let refreshControl = UIRefreshControl()
+    
     private let emptyLabel = UILabel().then {
         $0.text = "내용이 없습니다."
         $0.font = .systemFont(ofSize: 16)
@@ -29,7 +36,7 @@ class MainPostVC: BaseViewController, UITableViewDelegate, UITableViewDataSource
         $0.register(PostCell.self, forCellReuseIdentifier: "PostCell")
     }
 
-    private let dateLabel = UILabel().then {
+    let dateLabel = UILabel().then {
         $0.textColor = Asset.mainColor.color
         $0.font = .roundedFont(ofSize: 15, weight: .semibold)
         $0.textAlignment = .center
@@ -55,16 +62,58 @@ class MainPostVC: BaseViewController, UITableViewDelegate, UITableViewDataSource
         [labelBackView, dateLabel, postNameLabel, tableView, createPostFloatingButton].forEach {
             view.addSubview($0)
         }
+        
+        refreshControl.tintColor = Asset.labelColor.color
+        refreshControl.endRefreshing() // 초기화 - refresh 종료
+        tableView.refreshControl = refreshControl
 
-        dateLabel.text = "0000-00-00"
         dateLabel.text = dateFormatter.string(from: Date())
         postNameLabel.text = "학교 게시판"
         bind(reactor: reactor)
     }
-
+    
+    func bind(reactor: MainPostReactor) {
         createPostFloatingButton.rx.tap.bind {
             self.navigationController?.pushViewController(AddPostVC(), animated: true)
         }.disposed(by: disposeBag)
+        
+        refreshControl.rx.controlEvent(.valueChanged)
+            .map { Reactor.Action.refreshList }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+
+        rx.viewDidAppear
+            .map { _ in Reactor.Action.viewDidLoad }
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        tableView.rx.modelSelected(QuestionData.self)
+            .bind { [weak self] in
+                let nextVC = ReadPostVC()
+                self?.navigationController?.pushViewController(nextVC, animated: true)
+                nextVC.navigationItem.title = "\($0.writer.githubId)님의 개시물"
+                nextVC.dateLabel.text = "\($0.writer.githubId)님이 작성했어요"
+                nextVC.titleTextLabel.text = $0.title
+                nextVC.contentTextView.text = $0.content
+                nextVC.postId = $0.id
+            }.disposed(by: disposeBag)
+        
+        reactor.state
+                .map { $0.list }
+                .bind(to: tableView.rx.items(
+                        cellIdentifier: "PostCell",
+                        cellType: PostCell.self)) {(row, element: QuestionData, cell) in
+                            cell.firstLabel.text = element.title
+                            cell.secondLabel.text = element.writer.name
+                            cell.contentLabel.text = element.content
+
+                }.disposed(by: disposeBag)
+                
+        reactor.state
+            .map { $0.isLoading }
+            .delay(DispatchTimeInterval.milliseconds(500), scheduler: MainScheduler.instance)
+            .bind(to: refreshControl.rx.isRefreshing)
+            .disposed(by: disposeBag)
     }
     
     override func setupConstraints() {
