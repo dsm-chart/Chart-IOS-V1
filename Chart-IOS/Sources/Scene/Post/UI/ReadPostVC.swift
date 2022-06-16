d47ae1a5c8314b990d01c7debd729262b7346249
Daehee Kim <0824dh@naver.com>
2022-06-16 10:05:35 +0900

    🌧 :: ReadPost Dummy Data

--- Chart-IOS/Sources/Scene/Post/UI/ReadPostVC.swift
+++ Chart-IOS/Sources/Scene/Post/UI/ReadPostVC.swift
@@ -8,14 +8,14 @@ import UIKit
 
 class ReadPostVC: BaseViewController {
     
-    let postId = ""
+    var postId = ""
 
     let titleTextLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 20, weight: .semibold)
         $0.tintColor = Asset.mainColor.color
     }
 
-    private let dateLabel = UILabel().then {
+    let dateLabel = UILabel().then {
         $0.textColor = Asset.mainColor.color
         $0.font = .roundedFont(ofSize: 12, weight: .regular)
     }
@@ -38,10 +38,6 @@ class ReadPostVC: BaseViewController {
     override func configureUI() {
 
         // Dummy Data
-        navigationItem.title = "OOO님의 개시물"
-        titleTextLabel.text = "제목이 들어가요."
-        dateLabel.text = "0000-00-00 시간에 OOO님이 작성했어요."
-        contentTextView.text = "여기에는 본문이 들어가고 곧 삭제될 더미데이터 에여 ㅎㅎ"
 
         [titleTextLabel, dateLabel, contentTextView, goCommentButton].forEach {
             view.addSubview($0)
