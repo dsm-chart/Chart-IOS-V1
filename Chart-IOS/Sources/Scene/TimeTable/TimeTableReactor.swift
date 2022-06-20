3a26996e070cd32ce913ff8138a3025b0fa0bed7
Daehee Kim <0824dh@naver.com>
2022-06-20 19:04:10 +0900

    🌧 :: TimeTableReactor

--- Chart-IOS/Sources/Scene/TimeTable/TimeTableReactor.swift
+++ Chart-IOS/Sources/Scene/TimeTable/TimeTableReactor.swift
@@ -6,3 +6,66 @@
 //
 
 import Foundation
+import ReactorKit
+import RxSwift
+import RxCocoa
+import SPIndicator
+
+class TimeTableReactor: Reactor {
+    
+    let initialState = State(timeTable: [])
+    private let disposeBag: DisposeBag = .init()
+    
+    enum Action {
+        case viewDidLoad
+    }
+    enum Mutation {
+        case getTimaTable([TimeTable])
+    }
+    struct State {
+        var timeTable: [TimeTable]
+    }
+}
+
+// MARK: - Mutate
+extension TimeTableReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidLoad:
+            return getTimeTable().map { timeTable -> Mutation in
+                return .getTimaTable(timeTable)
+            }
+        }
+    }
+}
+
+// MARK: - Reduce
+extension TimeTableReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .getTimaTable(timeTable):
+            newState.timeTable = timeTable
+        }
+        return newState
+    }
+}
+
+// MARK: - Function
+
+extension TimeTableReactor {
+    func getTimeTable() -> PublishRelay<[TimeTable]> {
+        let timeTable = PublishRelay<[TimeTable]>()
+        API.getTimeTable.request().subscribe { event in
+            switch event {
+            case .success(let response):
+                if let data = try? JSONDecoder().decode(TimeTableResponse.self, from: response.data) {
+                    timeTable.accept(data.data)
+                }
+            case .failure(_):
+                SPIndicator.present(title: "Error!!", haptic: .error)
+            }
+        }.disposed(by: disposeBag)
+        return timeTable
+    }
+}
