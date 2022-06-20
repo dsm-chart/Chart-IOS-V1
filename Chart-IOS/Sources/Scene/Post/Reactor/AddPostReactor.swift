dff6a8a4933512ad2fe8d47692240bb0df94eddd
Daehee Kim <0824dh@naver.com>
2022-06-20 12:16:41 +0900

    🌧 :: AddPostReactor reduce

--- Chart-IOS/Sources/Scene/Post/Reactor/AddPostReactor.swift
+++ Chart-IOS/Sources/Scene/Post/Reactor/AddPostReactor.swift
@@ -18,12 +18,14 @@ class AddPostReactor: Reactor {
     
     enum Action {
         case postButtonClicked
+        case updateTitle(String)
+        case updateContent(String)
     }
     
     enum Mutation {
-        case setTitle
-        case setContent
-        case postQuestion
+        case setTitle(String)
+        case setContent(String)
+        case postQuestion(Bool)
     }
     
     struct State {
@@ -38,7 +40,47 @@ extension AddPostReactor {
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
         case .postButtonClicked:
-            return .just(.postQuestion)
+            let postQuestion = postQuestion(state: State()).map { bool -> Mutation in return .postQuestion(bool) }
+            return postQuestion
+        case .updateTitle(let title):
+            return .just(.setTitle(title))
+        case .updateContent(let content):
+            return .just(.setContent(content))
         }
+    }
+}
+
+// MARK: - Reduce
+extension AddPostReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .setTitle(title):
+            newState.title = title
+        case let .setContent(content):
+            newState.content = content
+        case .postQuestion: break
+        }
+        return newState
+    }
+}
+
+// MARK: - Function
+
+extension AddPostReactor {
+    func postQuestion(state: State) -> PublishRelay<Bool> {
+        let bool = PublishRelay<Bool>()
+        let parm = QuestionRequest(title: state.title, content: state.content)
+        
+        API.postQuestion(parm).request().subscribe { event in
+                switch event {
+                case .success(_):
+                    bool.accept(true)
+                case .failure(_):
+                    SPIndicator.present(title: "전송 실폐", haptic: .error)
+                    bool.accept(false)
+                }
+        }.disposed(by: disposeBag)
+        return bool
     }
 }
