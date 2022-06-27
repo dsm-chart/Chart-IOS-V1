53060834b7ffd0f4250c660d69ceb6840597ed4c
Daehee Kim <0824dh@naver.com>
2022-06-27 20:33:36 +0900

    🌧 ::  AddCommentReactor Mutate Function

--- Chart-IOS/Sources/Scene/Post/Reactor/AddCommentReactor.swift
+++ Chart-IOS/Sources/Scene/Post/Reactor/AddCommentReactor.swift
@@ -26,3 +26,33 @@ class AddCommentReactor: Reactor {
     struct State {}
 }
 
+// MARK: - Mutate
+extension AddCommentReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .postButtonClicked(postId, content):
+            return postComment(postId: postId, content: content)
+                    .map { bool -> Mutation in return .postComment }
+        }
+    }
+}
+
+// MARK: - Function
+
+extension AddCommentReactor {
+    func postComment(postId: String, content: String) -> PublishRelay<Bool> {
+        let bool = PublishRelay<Bool>()
+        let parm = CommentRequest(content: content, targetId: postId)
+
+        API.postComment(parm).request().subscribe { event in
+                    switch event {
+                    case .success(_):
+                        bool.accept(true)
+                    case .failure(_):
+                        SPIndicator.present(title: "전송 실폐", haptic: .error)
+                        bool.accept(false)
+                    }
+                }.disposed(by: disposeBag)
+        return bool
+    }
+}
