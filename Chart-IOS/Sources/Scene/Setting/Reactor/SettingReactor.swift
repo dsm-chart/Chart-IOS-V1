aa52c5ea5f0f824a4969ac6320b01b8045568d43
Daehee Kim <0824dh@naver.com>
2022-07-05 17:18:18 +0900

    🌧 ::  SettingReactor func

--- Chart-IOS/Sources/Scene/Setting/Reactor/SettingReactor.swift
+++ Chart-IOS/Sources/Scene/Setting/Reactor/SettingReactor.swift
@@ -27,3 +27,43 @@ class SettingReactor: Reactor {
             var myList: [QuestionData] = []
         }
 }
+
+// MARK: - Mutate
+extension SettingReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .viewDidLoad:
+            return getUserInfo().map { userInfo -> Mutation in
+                return .getUserInfo(userInfo.name, userInfo.questionList)
+            }
+        }
+    }
+}
+// MARK: - Reduce
+extension  SettingReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .getUserInfo(userName, myList):
+            newState.userName = userName
+            newState.myList = myList
+        }
+        return newState
+    }
+}
+// MARK: - Function
+extension  SettingReactor {
+    func getUserInfo() -> PublishRelay<MyInformation> {
+        let userInfo = PublishRelay<MyInformation>()
+        API.myAuth.request().subscribe { event in
+            switch event {
+            case .success(let response):
+                if let data = try? JSONDecoder().decode(MyInformation.self, from: response.data) {
+                    userInfo.accept(data)
+                }
+            case .failure(_): break
+            }
+        }.disposed(by: disposeBag)
+        return userInfo
+    }
+}
