69b5966a96a5b760e569e236a56289a7ea672703
Daehee Kim <0824dh@naver.com>
2022-07-10 22:37:34 +0900

    🌧 :: VC 앤티티 & 바인딩

--- Chart-IOS/Sources/Scene/Setting/UI/MyPostVC.swift
+++ Chart-IOS/Sources/Scene/Setting/UI/MyPostVC.swift
@@ -5,7 +5,36 @@
 //  Created by 김대희 on 2022/07/07.
 //
 
-import Foundation
+import UIKit
+import RxSwift
+
+class MyPostVC: BaseViewController {
+
+    var myList: [QuestionList] = []
+
+    private let tableView = UITableView().then {
+        $0.backgroundColor = Asset.backgroundColor.color
+        $0.register(PostCell.self, forCellReuseIdentifier: "PostCell")
+        $0.separatorStyle = .none
+    }
+
+    override func configureUI() {
+        navigationItem.title = "내가 작성한 게시물"
+
+        [tableView].forEach {
+            view.addSubview($0)
+        }
+
+        let list = Observable.just(myList)
+
+        list.bind(to: tableView.rx.items(
+            cellIdentifier: "PostCell",
+            cellType: PostCell.self)) {(row, element: QuestionList, cell) in
+                cell.firstLabel.text = element.title
+                cell.contentLabel.text = element.content
+        }.disposed(by: disposeBag)
+
+    }
     
     override func setLayout() {
         self.tabBarController?.tabBar.isHidden = true
